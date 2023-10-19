4bbcfb2330c846cfb5ca6a4f1f125275ff224c4d
sandeepReddy14 <60684412+sandeepReddy14@users.noreply.github.com>
2023-10-19 23:46:22 +0530

    OperatorsExample in Java with examples.

---
+++ Basics/src/OperatorsExample.java
@@ -0,0 +1,44 @@
+/*
+Operator in java is a symbol that is used to perform operations.
+There are many types of operators in Java
+1.Unary Operator:-
+The java unary operators require only one operand. Unary operators are used for
+->incrementing/decrementing a value by one (++, --, )
+->negating an expression(!)
+->inverting the value of a boolean(~)
+
+2.Arithmetic Operators:-
+The arithmetic operators are used to perform addition, subtraction, multiplication and division.
+(+, -, *, /)
+
+3.Bit-shift Operators:-
+In java bit-shift operators are used to shift bits of an operand to left or right.
+Left shift operator <<
+Right shift operator >>
+>>>, >> works same for positive number, but for negative number >>> changes parity bit (MSB) to 0.
+
+4.Relational Operators:-
+Relational operators are comparison and equality operators.
+<, >, <=, >=, instanceof are comparison operators.
+==, != are equality operators.
+
+5.Bitwise and Logical Operators:-
+The logical operators evalute an expression to true or false.
+&&, & are AND operator.
+||, | are OR operator.
+The logical &&, || operators don't check the second condition if the first condition is false.
+It checks the second condition only if the first one is true.
+
+The bitwise &, | operator checks both conditions whether first condition is true or false.
+^ is bitwise XOR
+
+6.Ternary Operators:-
+Ternary operator is one line replacement for if-the-else.
+(condition)?(statement1):(statement2)
+
+7.Assignment Operators:-
+Assignment operator is used to assign the value on its right to the operand on its left.
+(=)
+*/
+public class OperatorsExample {
+}
